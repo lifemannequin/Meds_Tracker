76ed82510cf25e292aeadcd505fe9cd1854de188
Veronica Taipe <46823698+lifemannequin@users.noreply.github.com>
2025-03-14 21:35:26 +0100

    Update meds_tracker.py

--- scripts/meds_tracker.py
+++ scripts/meds_tracker.py
@@ -70,6 +70,7 @@ except dropbox.exceptions.ApiError:
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt='%d-%m-%Y %H:%M',
     handlers=[
         logging.FileHandler(log_file, mode="a"),  # Append to log
         logging.StreamHandler(),
