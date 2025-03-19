3f869ab6e4559c45d70c245bb55262981927915e
Veronica Taipe <46823698+lifemannequin@users.noreply.github.com>
2025-03-19 18:42:44 +0100

    Update meds_tracker.py

--- scripts/meds_tracker.py
+++ scripts/meds_tracker.py
@@ -278,7 +278,7 @@ for index, row in data.iterrows():
     remaining_days = int((row["N_pills"] - pills_taken[index])/row['pills_per_day'])
     
     if remaining_days == 10:
-        email = jmespath.search(f" [?name == '{row['Acc_name']}'].email",accounts_info)
+        email = jmespath.search(f"Accounts[?name == '{row['Acc_name']}'].email",accounts_info)
         if is_valid_email(email[0]):
             body = f"Reminder: You have only {remaining_days} days left for {row['Med_name']}."
             send_email(email[0], f"Medication Reminder: {row['Med_name']}", body,creds)
