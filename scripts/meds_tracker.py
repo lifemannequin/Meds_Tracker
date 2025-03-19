046c18607fd42b61ca7c09f8d3e630ff4cc92e70
Veronica Taipe <46823698+lifemannequin@users.noreply.github.com>
2025-03-19 21:49:30 +0100

    Update meds_tracker.py

--- scripts/meds_tracker.py
+++ scripts/meds_tracker.py
@@ -18,6 +18,7 @@ import dropbox
 from dropbox.exceptions import ApiError, HttpError
 import jmespath
 from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from email.mime.text import MIMEText
